626d5d54a07866ddf50db36dd1ace98c586bc99b
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2022-10-10 11:07:23 -0400

    add some preliminary results for compass model

---
+++ scripts/compass_example.jl
@@ -0,0 +1,109 @@
+# Author: Mathias Louboutin
+# Date: June 2021
+#
+
+using DrWatson
+@quickactivate "Amortized-FNO-VC"
+using JUDI, LinearAlgebra, Images, PyPlot, DSP, ImageGather, Printf
+using JLD2
+using SlimPlotting
+using Random
+using Distributions
+Random.seed!(2022)
+
+include(srcdir("utils.jl"))
+JLD2.@load "../data/BGCompass_tti_625m.jld2" n d o m;
+extent = ((n[1]-1)*d[1], (n[2]-1)*d[2])
+
+# Velocity [km/s]
+v =  1f0./sqrt.(m);
+idx_wb = minimum(find_water_bottom(v.-v[1,1]))
+function gaussian_background(m::Matrix{Float32}, width::Union{Number, Tuple{Number, Number}}; idx_wb=idx_wb)
+    v =  1f0./sqrt.(m);
+    v0 = deepcopy(v);
+    v0[:,idx_wb+1:end] = imfilter(v[:,idx_wb+1:end], Kernel.gaussian(width))
+    m0 = (1f0 ./ v0).^2
+    return m0
+end
+
+# background model
+init_width = 10;
+m0 = gaussian_background(m, init_width);
+
+# Setup info and model structure
+nsrc = 16	# number of sources
+model = Model(n, d, o, m; nb=40)
+model0 = Model(n, d, o, m0; nb=40)
+
+# Set up receiver geometry
+nxrec = n[1]
+xrec = range(0f0, stop=(n[1] -1)*d[1], length=nxrec)
+yrec = 0f0
+#zrec = range((idx_wb-1)*d[2], stop=(idx_wb-1)*d[2], length=nxrec)
+zrec = range(10f0, stop=10f0, length=nxrec)
+
+# receiver sampling and recording time
+timeD = 1800f0   # receiver recording time [ms]
+dtD = 4f0    # receiver sampling interval [ms]
+
+# Set up receiver structure
+recGeometry = Geometry(xrec, yrec, zrec; dt=dtD, t=timeD, nsrc=nsrc)
+# Set up source geometry (cell array with source locations for each shot)
+xsrc = range(0f0, stop=(n[1] -1)*d[1], length=nsrc)
+ysrc = range(0f0, stop=0f0, length=nsrc)
+zsrc = range(10f0, stop=10f0, length=nsrc)
+
+# Set up source structure
+srcGeometry = Geometry(convertToCell(xsrc), convertToCell(ysrc), convertToCell(zsrc); dt=dtD, t=timeD)
+# setup wavelet
+f0 = 0.025f0     # kHz
+wavelet = ricker_wavelet(timeD, dtD, f0)
+q = judiVector(srcGeometry, wavelet)
+
+###################################################################################################
+opt = Options(isic=true)
+
+# Setup operators
+F = judiModeling(model, srcGeometry, recGeometry; options=opt)
+J = judiJacobian(F(model0), q)
+
+# Nonlinear modeling
+@time dobs = F * q
+@time rtm = J'*dobs
+
+# Preconditioners
+Mr = judiTopmute(n, idx_wb, 1) * judiDepthScaling(model0) * judiDepthScaling(model0)
+
+exp_config = @strdict nsrc nxrec n d init_width
+fig = figure(figsize=(20,12))
+plot_simage(reshape(Mr * rtm, n)', d; d_scale=0, new_fig=false, name="initial RTM")
+tight_layout();
+safesave(joinpath(plotsdir("compass-example"),savename(exp_config; digits=6)*"_init.png"), fig); 
+close(fig);
+
+for i = 1:10
+    m0 = gen_m0_vary(m)
+
+    fig = figure(figsize=(20,12))
+    plot_velocity(m0', d; new_fig=false, name="background model")
+    tight_layout();
+    safesave(joinpath(plotsdir("compass-example"),savename(exp_config; digits=6)*"_background.png"), fig); 
+    close(fig);
+end
+
+for i = 1:5
+
+    exp_config = @strdict nsrc nxrec n d
+
+    m0 = gen_m0_vary(m)
+
+    J = judiJacobian(F(; m=m0), q)
+    @time rtm = J'*dobs
+
+    fig = figure(figsize=(20,12))
+    plot_simage(reshape(Mr * rtm, n)', d; d_scale=0, new_fig=false, name="RTM")
+    tight_layout();
+    safesave(joinpath(plotsdir("compass-example"),savename(exp_config; digits=6)*"_continued.png"), fig); 
+    close(fig);
+    
+end
---
+++ src/utils.jl
@@ -0,0 +1,58 @@
+function λtoΣ(λ::Vector{Float32})
+    A = randn(Float32,2,2)
+    Q, _ = qr(A)
+    return Q' * diagm(λ) * Q
+end
+function gaussianfunction(x, y, Σ::Matrix{Float32})
+    return .5f0/pi/√(abs(det(Σ))) * exp(-0.5f0 * dot([x, y], Σ\[x;y]))
+end
+function gaussiankernel(Σ::Matrix{Float32})
+    width = (40,40)
+    A = zeros(Float32, width)
+    
+    for i = 1:width[1]
+        for j = 1:width[2]
+            A[i,j] = gaussianfunction(i-(width[1]+1)/2f0, j-(width[2]+1)/2f0, Σ)
+        end
+    end
+    return A/sum(A)
+end
+function gaussian_background(m::Matrix{Float32}, width::Union{Number, Tuple{Number, Number}}; idx_wb=idx_wb)
+    v =  1f0./sqrt.(m);
+    v0 = deepcopy(v);
+    v0[:,idx_wb+1:end] = imfilter(v[:,idx_wb+1:end], Kernel.gaussian(width))
+    m0 = (1f0 ./ v0).^2
+    return m0
+end
+function gaussian_background(m::Matrix{Float32}, Σ::Matrix{Float32}; idx_wb=idx_wb)
+    v =  1f0./sqrt.(m);
+    v0 = deepcopy(v);
+    v0[:,idx_wb+1:end] = imfilter(v[:,idx_wb+1:end], gaussiankernel(Σ))
+    m0 = (1f0 ./ v0).^2
+    return m0
+end
+function gen_m0(m; cut_rows=100, idx_wb=idx_wb)
+    keep_rows = deleteat!(collect(idx_wb+1:size(m,2)), sort(randperm(size(m,2)-idx_wb)[1:cut_rows]))
+    m0 = hcat(m[:,1:idx_wb],imresize(m[:,keep_rows], (size(m,1), size(m,2)-idx_wb)))
+    return gaussian_background(m0, 20)
+end
+function gen_m0_vary(m; idx_wb=idx_wb, d=d)
+    n = size(m)
+    X = convert(Array{Float32},reshape(range(0f0,stop=(n[1]-1)*d[1],length=n[1]),:,1))
+    Cova = gaussian_kernel(X,X',theta0=5,delta=250,cons=1f-5)
+    cutlength = rand(MvNormal(zeros(Float32,n[1]),Cova))
+    cutlength = Int.(round.(cutlength/norm(cutlength) * 500 .+ 80))
+    start_cut = rand(idx_wb+1:n[2]-maximum(cutlength))
+    keep_idx = [deleteat!(collect(idx_wb+1:n[2]), start_cut-idx_wb:start_cut+cutlength[i]-1-idx_wb) for i = 1:n[1]]
+    #λ = Float32((maximum(m)-minimum(m))/2/π * rand(Float32))
+    m0 = vcat([vcat(m[i,1:idx_wb],imresize(m[i,keep_idx[i]], n[2]-idx_wb)) for i = 1:n[1]]'...)
+    #m0 = vcat([vcat(m[i,1:idx_wb],v_m(skew.(m_v(imresize(m[i,keep_idx[i]], n[2]-idx_wb)), λ)))' for i = 1:n[1]]...)
+    return gaussian_background(m0, 20)
+end
+function gaussian_kernel(xa,xy;theta0=1,delta=1,cons=1f-5)
+    return theta0*exp.(-(xa.-xy).^2f0*1f0/delta^2f0)+theta0*cons*I
+end
+m_v(m::Array{Float32}) = 1f0./sqrt.(m)
+v_m(v::Array{Float32}) = 1f0./v.^2f0
+skew(x, λ::Float32, _min::Float32, _max::Float32) =  λ * Float32(sin(2 * π * (x-_min)/(_max-_min))) + x
+skew(x, λ::Float32) = skew(x,λ,minimum(x),maximum(x))
